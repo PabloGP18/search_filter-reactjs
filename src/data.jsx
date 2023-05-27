22759fbfe9d16b7a2c9c04fd92227f4bf33f0cf7
Pablo GP <garciaplaza.pablo@gmail.com>
2023-05-27 05:00:29 +0200

    Mockdata

---
+++ src/data.jsx
@@ -0,0 +1,702 @@
+export const data = [
+  {
+    id: 1,
+    first_name: 'Kat',
+    last_name: 'Pike',
+    email: 'kpike0@e-recht24.de',
+    phone: '919-896-3925'
+  },
+  {
+    id: 2,
+    first_name: 'Chaim',
+    last_name: 'Keep',
+    email: 'ckeep1@ycombinator.com',
+    phone: '642-290-6108'
+  },
+  {
+    id: 3,
+    first_name: 'Kennett',
+    last_name: 'Clinch',
+    email: 'kclinch2@tumblr.com',
+    phone: '743-789-4131'
+  },
+  {
+    id: 4,
+    first_name: 'Michaelina',
+    last_name: 'Millichap',
+    email: 'mmillichap3@aol.com',
+    phone: '239-834-9002'
+  },
+  {
+    id: 5,
+    first_name: 'Harlene',
+    last_name: 'Gooden',
+    email: 'hgooden4@ovh.net',
+    phone: '735-811-7392'
+  },
+  {
+    id: 6,
+    first_name: 'Peter',
+    last_name: 'Martignoni',
+    email: 'pmartignoni5@upenn.edu',
+    phone: '119-110-0959'
+  },
+  {
+    id: 7,
+    first_name: 'Arch',
+    last_name: 'Vanyushkin',
+    email: 'avanyushkin6@purevolume.com',
+    phone: '265-264-1973'
+  },
+  {
+    id: 8,
+    first_name: 'Trevar',
+    last_name: 'Chittim',
+    email: 'tchittim7@boston.com',
+    phone: '665-758-6521'
+  },
+  {
+    id: 9,
+    first_name: 'Benedicto',
+    last_name: 'Redsall',
+    email: 'bredsall8@go.com',
+    phone: '841-959-0872'
+  },
+  {
+    id: 10,
+    first_name: 'Giavani',
+    last_name: 'Brownsword',
+    email: 'gbrownsword9@ihg.com',
+    phone: '449-368-3511'
+  },
+  {
+    id: 11,
+    first_name: 'Raquel',
+    last_name: 'Fishby',
+    email: 'rfishbya@freewebs.com',
+    phone: '302-266-0760'
+  },
+  {
+    id: 12,
+    first_name: 'Symon',
+    last_name: 'Cucuzza',
+    email: 'scucuzzab@google.it',
+    phone: '205-974-0507'
+  },
+  {
+    id: 13,
+    first_name: 'Darelle',
+    last_name: 'Farrand',
+    email: 'dfarrandc@i2i.jp',
+    phone: '951-116-4839'
+  },
+  {
+    id: 14,
+    first_name: 'Cammi',
+    last_name: 'Giriardelli',
+    email: 'cgiriardellid@themeforest.net',
+    phone: '229-334-5652'
+  },
+  {
+    id: 15,
+    first_name: 'Lilia',
+    last_name: 'Le Provest',
+    email: 'lleproveste@elegantthemes.com',
+    phone: '715-769-1728'
+  },
+  {
+    id: 16,
+    first_name: 'Hazlett',
+    last_name: 'Aspole',
+    email: 'haspolef@yolasite.com',
+    phone: '342-753-8876'
+  },
+  {
+    id: 17,
+    first_name: 'Brian',
+    last_name: 'Beatey',
+    email: 'bbeateyg@hp.com',
+    phone: '222-479-1880'
+  },
+  {
+    id: 18,
+    first_name: 'Asher',
+    last_name: 'Duthie',
+    email: 'aduthieh@cnbc.com',
+    phone: '275-224-7557'
+  },
+  {
+    id: 19,
+    first_name: 'Vernice',
+    last_name: 'Reicharz',
+    email: 'vreicharzi@unblog.fr',
+    phone: '272-819-8437'
+  },
+  {
+    id: 20,
+    first_name: 'Melvin',
+    last_name: 'Swainsbury',
+    email: 'mswainsburyj@google.com.au',
+    phone: '751-487-6130'
+  },
+  {
+    id: 21,
+    first_name: 'Corri',
+    last_name: 'Moller',
+    email: 'cmollerk@nytimes.com',
+    phone: '849-111-0109'
+  },
+  {
+    id: 22,
+    first_name: 'Vanni',
+    last_name: 'Ledgeway',
+    email: 'vledgewayl@mashable.com',
+    phone: '781-651-8736'
+  },
+  {
+    id: 23,
+    first_name: 'Anette',
+    last_name: 'Weavers',
+    email: 'aweaversm@earthlink.net',
+    phone: '360-611-9522'
+  },
+  {
+    id: 24,
+    first_name: 'Griselda',
+    last_name: 'Hayhow',
+    email: 'ghayhown@histats.com',
+    phone: '242-954-9113'
+  },
+  {
+    id: 25,
+    first_name: 'Kati',
+    last_name: 'Alvar',
+    email: 'kalvaro@time.com',
+    phone: '515-802-2899'
+  },
+  {
+    id: 26,
+    first_name: 'Artemis',
+    last_name: 'Fransman',
+    email: 'afransmanp@skyrock.com',
+    phone: '103-730-9324'
+  },
+  {
+    id: 27,
+    first_name: 'Almira',
+    last_name: 'Aim',
+    email: 'aaimq@uol.com.br',
+    phone: '463-669-1979'
+  },
+  {
+    id: 28,
+    first_name: 'Elwin',
+    last_name: 'Elesander',
+    email: 'eelesanderr@imageshack.us',
+    phone: '472-751-0167'
+  },
+  {
+    id: 29,
+    first_name: 'Neil',
+    last_name: 'Tayt',
+    email: 'ntayts@ifeng.com',
+    phone: '245-923-7681'
+  },
+  {
+    id: 30,
+    first_name: 'Junette',
+    last_name: 'Fallis',
+    email: 'jfallist@usgs.gov',
+    phone: '460-745-4168'
+  },
+  {
+    id: 31,
+    first_name: 'Alejandro',
+    last_name: 'Andrzej',
+    email: 'aandrzeju@wp.com',
+    phone: '875-586-6176'
+  },
+  {
+    id: 32,
+    first_name: 'Dane',
+    last_name: 'McRannell',
+    email: 'dmcrannellv@flickr.com',
+    phone: '661-132-2469'
+  },
+  {
+    id: 33,
+    first_name: 'Debora',
+    last_name: 'Ruzicka',
+    email: 'druzickaw@plala.or.jp',
+    phone: '479-557-6170'
+  },
+  {
+    id: 34,
+    first_name: 'Dodie',
+    last_name: 'Godsmark',
+    email: 'dgodsmarkx@elpais.com',
+    phone: '718-734-8991'
+  },
+  {
+    id: 35,
+    first_name: 'Vassily',
+    last_name: 'Keyes',
+    email: 'vkeyesy@techcrunch.com',
+    phone: '269-815-2146'
+  },
+  {
+    id: 36,
+    first_name: 'Carl',
+    last_name: 'Pattini',
+    email: 'cpattiniz@comcast.net',
+    phone: '528-910-7125'
+  },
+  {
+    id: 37,
+    first_name: 'Ginnie',
+    last_name: 'Coughan',
+    email: 'gcoughan10@imgur.com',
+    phone: '378-113-6755'
+  },
+  {
+    id: 38,
+    first_name: 'Tedd',
+    last_name: 'Durbridge',
+    email: 'tdurbridge11@mozilla.com',
+    phone: '866-179-3306'
+  },
+  {
+    id: 39,
+    first_name: 'Shannen',
+    last_name: 'Abbott',
+    email: 'sabbott12@wix.com',
+    phone: '243-308-6708'
+  },
+  {
+    id: 40,
+    first_name: 'Yettie',
+    last_name: 'Whisson',
+    email: 'ywhisson13@dailymotion.com',
+    phone: '275-689-0763'
+  },
+  {
+    id: 41,
+    first_name: 'Archaimbaud',
+    last_name: 'Dixsee',
+    email: 'adixsee14@google.cn',
+    phone: '595-290-9595'
+  },
+  {
+    id: 42,
+    first_name: 'Pearce',
+    last_name: 'Waszczykowski',
+    email: 'pwaszczykowski15@bbb.org',
+    phone: '952-998-5387'
+  },
+  {
+    id: 43,
+    first_name: 'Kelsi',
+    last_name: 'Nairne',
+    email: 'knairne16@ezinearticles.com',
+    phone: '489-469-4737'
+  },
+  {
+    id: 44,
+    first_name: 'Vittorio',
+    last_name: 'McRae',
+    email: 'vmcrae17@indiatimes.com',
+    phone: '868-674-0023'
+  },
+  {
+    id: 45,
+    first_name: 'Willy',
+    last_name: 'Hugk',
+    email: 'whugk18@reddit.com',
+    phone: '851-371-6298'
+  },
+  {
+    id: 46,
+    first_name: 'Guillermo',
+    last_name: "O'Corrane",
+    email: 'gocorrane19@blogger.com',
+    phone: '140-270-3522'
+  },
+  {
+    id: 47,
+    first_name: 'Jacqueline',
+    last_name: 'Foy',
+    email: 'jfoy1a@earthlink.net',
+    phone: '434-738-8578'
+  },
+  {
+    id: 48,
+    first_name: 'Kerrie',
+    last_name: 'Casperri',
+    email: 'kcasperri1b@geocities.com',
+    phone: '498-804-7075'
+  },
+  {
+    id: 49,
+    first_name: 'Grant',
+    last_name: 'Shorto',
+    email: 'gshorto1c@foxnews.com',
+    phone: '443-500-5397'
+  },
+  {
+    id: 50,
+    first_name: 'Grange',
+    last_name: 'Deetlof',
+    email: 'gdeetlof1d@1688.com',
+    phone: '858-267-0141'
+  },
+  {
+    id: 51,
+    first_name: 'Patrizia',
+    last_name: 'Lawie',
+    email: 'plawie1e@exblog.jp',
+    phone: '910-643-9246'
+  },
+  {
+    id: 52,
+    first_name: 'Timmie',
+    last_name: 'Sailer',
+    email: 'tsailer1f@360.cn',
+    phone: '801-992-1454'
+  },
+  {
+    id: 53,
+    first_name: 'Dianna',
+    last_name: 'Yakobowitz',
+    email: 'dyakobowitz1g@patch.com',
+    phone: '644-432-5467'
+  },
+  {
+    id: 54,
+    first_name: 'Katee',
+    last_name: 'Goodhay',
+    email: 'kgoodhay1h@unicef.org',
+    phone: '333-271-0857'
+  },
+  {
+    id: 55,
+    first_name: 'Cozmo',
+    last_name: 'Cowperthwaite',
+    email: 'ccowperthwaite1i@gizmodo.com',
+    phone: '759-856-8161'
+  },
+  {
+    id: 56,
+    first_name: 'Freddi',
+    last_name: 'Trail',
+    email: 'ftrail1j@tripadvisor.com',
+    phone: '432-735-3672'
+  },
+  {
+    id: 57,
+    first_name: 'Regina',
+    last_name: 'Simanek',
+    email: 'rsimanek1k@comcast.net',
+    phone: '770-278-9622'
+  },
+  {
+    id: 58,
+    first_name: 'Pooh',
+    last_name: 'Morman',
+    email: 'pmorman1l@disqus.com',
+    phone: '973-728-4458'
+  },
+  {
+    id: 59,
+    first_name: 'Nikolas',
+    last_name: "O'Hederscoll",
+    email: 'nohederscoll1m@hugedomains.com',
+    phone: '969-328-7413'
+  },
+  {
+    id: 60,
+    first_name: 'Leilah',
+    last_name: 'Coils',
+    email: 'lcoils1n@last.fm',
+    phone: '806-169-5058'
+  },
+  {
+    id: 61,
+    first_name: 'Kimbell',
+    last_name: 'Worgan',
+    email: 'kworgan1o@addtoany.com',
+    phone: '265-814-1573'
+  },
+  {
+    id: 62,
+    first_name: 'Ashia',
+    last_name: 'Sarton',
+    email: 'asarton1p@alibaba.com',
+    phone: '828-202-5621'
+  },
+  {
+    id: 63,
+    first_name: 'Armin',
+    last_name: 'Kerwick',
+    email: 'akerwick1q@technorati.com',
+    phone: '719-398-2161'
+  },
+  {
+    id: 64,
+    first_name: 'Francine',
+    last_name: 'Westphalen',
+    email: 'fwestphalen1r@artisteer.com',
+    phone: '687-865-9266'
+  },
+  {
+    id: 65,
+    first_name: 'Tamarah',
+    last_name: 'Laden',
+    email: 'tladen1s@chicagotribune.com',
+    phone: '908-810-0333'
+  },
+  {
+    id: 66,
+    first_name: 'Deb',
+    last_name: 'Fothergill',
+    email: 'dfothergill1t@home.pl',
+    phone: '574-175-3510'
+  },
+  {
+    id: 67,
+    first_name: 'Rodge',
+    last_name: 'Petruk',
+    email: 'rpetruk1u@hao123.com',
+    phone: '278-941-9866'
+  },
+  {
+    id: 68,
+    first_name: 'Nye',
+    last_name: 'Killoran',
+    email: 'nkilloran1v@google.nl',
+    phone: '132-732-5419'
+  },
+  {
+    id: 69,
+    first_name: 'Tonye',
+    last_name: 'Inderwick',
+    email: 'tinderwick1w@va.gov',
+    phone: '129-989-3526'
+  },
+  {
+    id: 70,
+    first_name: 'Lauralee',
+    last_name: 'Keller',
+    email: 'lkeller1x@themeforest.net',
+    phone: '762-609-6954'
+  },
+  {
+    id: 71,
+    first_name: 'Madalyn',
+    last_name: 'Egle of Germany',
+    email: 'megleofgermany1y@reference.com',
+    phone: '673-632-2450'
+  },
+  {
+    id: 72,
+    first_name: 'Ashlen',
+    last_name: 'Paddington',
+    email: 'apaddington1z@blogspot.com',
+    phone: '504-747-2882'
+  },
+  {
+    id: 73,
+    first_name: 'Francklyn',
+    last_name: 'Hacker',
+    email: 'fhacker20@cmu.edu',
+    phone: '130-471-5946'
+  },
+  {
+    id: 74,
+    first_name: 'Rodd',
+    last_name: 'Ringe',
+    email: 'rringe21@google.com',
+    phone: '921-184-1870'
+  },
+  {
+    id: 75,
+    first_name: 'Poul',
+    last_name: 'Cowdry',
+    email: 'pcowdry22@nytimes.com',
+    phone: '280-986-0454'
+  },
+  {
+    id: 76,
+    first_name: 'Amble',
+    last_name: 'Van Geffen',
+    email: 'avangeffen23@blogspot.com',
+    phone: '212-542-4817'
+  },
+  {
+    id: 77,
+    first_name: 'Xerxes',
+    last_name: 'Screeton',
+    email: 'xscreeton24@netscape.com',
+    phone: '255-869-9310'
+  },
+  {
+    id: 78,
+    first_name: 'Cornelia',
+    last_name: 'McFeate',
+    email: 'cmcfeate25@baidu.com',
+    phone: '965-445-4975'
+  },
+  {
+    id: 79,
+    first_name: 'Shannon',
+    last_name: 'Bremmer',
+    email: 'sbremmer26@tinyurl.com',
+    phone: '759-237-0418'
+  },
+  {
+    id: 80,
+    first_name: 'Ardelle',
+    last_name: 'Frichley',
+    email: 'africhley27@reddit.com',
+    phone: '165-835-5929'
+  },
+  {
+    id: 81,
+    first_name: 'Rodi',
+    last_name: 'Pendlebury',
+    email: 'rpendlebury28@europa.eu',
+    phone: '677-876-2611'
+  },
+  {
+    id: 82,
+    first_name: 'Elia',
+    last_name: 'Warwicker',
+    email: 'ewarwicker29@ycombinator.com',
+    phone: '854-902-4446'
+  },
+  {
+    id: 83,
+    first_name: 'Edan',
+    last_name: 'Pigeram',
+    email: 'epigeram2a@360.cn',
+    phone: '896-543-7905'
+  },
+  {
+    id: 84,
+    first_name: 'Ana',
+    last_name: 'Dirr',
+    email: 'adirr2b@altervista.org',
+    phone: '565-660-0737'
+  },
+  {
+    id: 85,
+    first_name: 'Simeon',
+    last_name: 'Diplock',
+    email: 'sdiplock2c@arizona.edu',
+    phone: '377-263-9269'
+  },
+  {
+    id: 86,
+    first_name: 'Orel',
+    last_name: 'Pipworth',
+    email: 'opipworth2d@shutterfly.com',
+    phone: '439-503-4324'
+  },
+  {
+    id: 87,
+    first_name: 'Martie',
+    last_name: 'Diggin',
+    email: 'mdiggin2e@purevolume.com',
+    phone: '424-667-5015'
+  },
+  {
+    id: 88,
+    first_name: 'Sissie',
+    last_name: 'Mitkov',
+    email: 'smitkov2f@harvard.edu',
+    phone: '905-111-8684'
+  },
+  {
+    id: 89,
+    first_name: 'Webster',
+    last_name: 'Feragh',
+    email: 'wferagh2g@cbsnews.com',
+    phone: '352-509-4621'
+  },
+  {
+    id: 90,
+    first_name: 'Hillyer',
+    last_name: 'Stratford',
+    email: 'hstratford2h@mashable.com',
+    phone: '500-641-9532'
+  },
+  {
+    id: 91,
+    first_name: 'Savina',
+    last_name: 'Minillo',
+    email: 'sminillo2i@altervista.org',
+    phone: '535-470-5623'
+  },
+  {
+    id: 92,
+    first_name: 'Ella',
+    last_name: 'Trevaskis',
+    email: 'etrevaskis2j@nytimes.com',
+    phone: '390-427-4412'
+  },
+  {
+    id: 93,
+    first_name: 'Darelle',
+    last_name: 'McGillreich',
+    email: 'dmcgillreich2k@ibm.com',
+    phone: '832-254-7060'
+  },
+  {
+    id: 94,
+    first_name: 'Carmita',
+    last_name: 'Leijs',
+    email: 'cleijs2l@edublogs.org',
+    phone: '392-531-9895'
+  },
+  {
+    id: 95,
+    first_name: 'Eben',
+    last_name: 'Week',
+    email: 'eweek2m@unc.edu',
+    phone: '975-824-5366'
+  },
+  {
+    id: 96,
+    first_name: 'Hortense',
+    last_name: 'De Courtney',
+    email: 'hdecourtney2n@blogspot.com',
+    phone: '454-313-1108'
+  },
+  {
+    id: 97,
+    first_name: 'Kathryn',
+    last_name: 'Filgate',
+    email: 'kfilgate2o@cyberchimps.com',
+    phone: '623-842-9591'
+  },
+  {
+    id: 98,
+    first_name: 'Viole',
+    last_name: 'Wyleman',
+    email: 'vwyleman2p@europa.eu',
+    phone: '238-962-1369'
+  },
+  {
+    id: 99,
+    first_name: 'Arlee',
+    last_name: 'Roset',
+    email: 'aroset2q@fastcompany.com',
+    phone: '733-999-9522'
+  },
+  {
+    id: 100,
+    first_name: 'Clair',
+    last_name: 'Cawtheray',
+    email: 'ccawtheray2r@dion.ne.jp',
+    phone: '524-872-1147'
+  }
+]
